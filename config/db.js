d6fe87fae311793e285a63d64f7cfb6dc2ce02bc
Rohit Sahu <sahu86744@gmail.com>
2023-11-04 08:44:50 +0530

    update timezone

--- config/db.js
+++ config/db.js
@@ -2,16 +2,16 @@
 const mongoose = require("mongoose")
 const cron = require("node-cron");
 const Event = require("../models/upcoming-event.model");
-const moment = require('moment');
+const moment = require('moment-timezone');
 require("dotenv").config()
 const winston = require('winston');
 
 const connection = mongoose.connect(process.env.mongo_url);
 cron.schedule('59 23 * * *', async () => {
-    // cron.schedule("*/6 * * * * *", async () => {
+    // cron.schedule("*/1 * * * * *", async () => {
     const currentDate = moment().tz('Asia/Kolkata').format("YYYY-MM-DD");
 
-
+    console.log("Current", currentDate)
     try {
         const pastEvents = await Event.find({ isActive: true, eventDate: { $lt: currentDate } });
 
